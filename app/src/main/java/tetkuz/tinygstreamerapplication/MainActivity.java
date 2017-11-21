5affb174bc5784d37ea57e08ac63429a782c9f6e
tetkuz <r3108.sh@gmail.com>
2017-11-21 17:04:36 +0900

    UIの実装

--- app/src/main/java/tetkuz/tinygstreamerapplication/MainActivity.java
+++ app/src/main/java/tetkuz/tinygstreamerapplication/MainActivity.java
@@ -2,12 +2,31 @@ package tetkuz.tinygstreamerapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import org.freedesktop.gstreamer.GStreamer;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        try {
+            GStreamer.init(this);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
         setContentView(R.layout.activity_main);
+
+        ((TextView) findViewById(R.id.label)).setText(String.format("Welcome %s !", nativeGetGStreamerInfo()));
     }
+
+    static {
+        System.loadLibrary("gstreamer_android");
+        System.loadLibrary("media_pipeline");
+    }
+
+    native final String nativeGetGStreamerInfo();
 }
